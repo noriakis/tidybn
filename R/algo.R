b2508acbeb42060243ed752bd01e5ae31bfde2ce
noriakis <nori@hgc.jp>
2023-08-21 22:55:16 +0900

    populating algo

--- R/algo.R
+++ R/algo.R
@@ -1,3 +1,5 @@
+## score-based
+
 #' hc
 #' 
 #' Hill-Climbing algorithm using bnlearn() for tibble
@@ -91,6 +93,8 @@ tabu.tbl_df <- function (x, start=NULL, whitelist=NULL,
     return(tblg)
 }
 
+
+## Hybrid
 
 #' rsmax2
 #' 
@@ -187,6 +191,45 @@ h2pc.tbl_df <- function(x, whitelist = NULL, blacklist = NULL, restrict.args = l
     res <- bnlearn::h2pc(df, whitelist=whitelist, blacklist=blacklist,
                  restrict.args=restrict.args,
                  maximize.args=maximize.args, debug=debug)
+
+    tblg <- res |> bnlearn::as.igraph() |> tidygraph::as_tbl_graph()
+    
+    params <- names(res$learning$args)
+    nodes <- names(res$nodes)
+    avg.mb <- mean(sapply(nodes, function(n) { length(res$nodes[[n]]$mb) }))
+    avg.nbr <- mean(sapply(nodes, function(n) { length(res$nodes[[n]]$nbr) }))
+    avg.ch <- mean(sapply(nodes, function(n) { length(res$nodes[[n]]$children) }))
+    attr(tblg, "avg.mb") <- avg.mb 
+    attr(tblg, "avg.nbr") <- avg.nbr 
+    attr(tblg, "avg.ch") <- avg.ch
+    for (at in res$learning |> names()) {
+        attr(tblg, at) <- res$learning[[at]]
+    }
+    return(tblg)       
+}
+
+
+## Constraint-based
+
+#'
+#' @export
+pc.stable <- function (...) 
+{
+    UseMethod("pc.stable")
+}
+
+#' @export
+pc.stable.tbl_df <- function(x, cluster, whitelist = NULL, blacklist = NULL, test = NULL,
+  alpha = 0.05, B = NULL, max.sx = NULL, debug = FALSE, undirected = FALSE) {
+    df <- x |> as.data.frame()
+    res <- bnlearn::pc.stable(df, cluster=cluster, whitelist=whitelist,
+    	        blacklist=blacklist,
+    	        test=test,
+    	        alpha=alpha,
+    	        B=B,
+    	        max.sx=max.sx,
+    	        debug=debug,
+    	        undirected=undirected)
 
     tblg <- res |> bnlearn::as.igraph() |> tidygraph::as_tbl_graph()
     
